32328abce97f6dd50a6204a3dac4d7d90090a8ed
Peter Levinsky <pele@easj.dk>
2020-08-27 09:51:57 +0200

    Add project files.

---
+++ ClassDemoTCPServer/ServerWorker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClassDemoTCPServer
+{
+    class ServerWorker
+    {
+        public void Start()
+        {
+            // her kommer koden
+
+            // opret server
+            // ip egen computer (samme som 127.0.0.1), port er applikationen her en ekko server derfor port 7
+            TcpListener server = new TcpListener(IPAddress.Loopback, 7);
+            server.Start();
+
+            // venter på en klient skal lave et opkald
+            TcpClient socket = server.AcceptTcpClient();
+
+            StreamReader sr = new StreamReader(socket.GetStream());
+            StreamWriter sw = new StreamWriter(socket.GetStream());
+
+            // læs tekst fra klient
+            String str = sr.ReadLine();
+            Console.WriteLine($"Her er server input: {str}");
+
+            // skriv tilbage til klient
+            String UpperStr = str.ToUpper();
+            sw.WriteLine(UpperStr);
+            sw.Flush(); // tømmer buffer
+
+            socket.Close();
+
+
+        }
+    }
+}
